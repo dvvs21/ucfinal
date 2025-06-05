a602bd50f8075beead3ebbc9d22cc69c21129172
dvvs21 <dvvssantos@outlook.com>
2025-06-04 21:38:19 -0300

    aula do dia 04/06

--- aluno/admin.py
+++ aluno/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from aluno.models import aluno
 
 # Register your models here.
-admin.site.register(aluno)
+admin.site.register(aluno)  
--- aluno/models.py
+++ aluno/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class aluno(models.Model):
     matricula = models.AutoField(primary_key=True, help_text="Código da Matrícula do Aluno")
-    dtinicial = models.DateField(help_text="Informe a Data de Início do Aluno")
-    dtfinal = models.DateField(help_text="Informe a Data Final do Aluno")
+    dtinicial = models.DateField(help_text="Informe a Data de Início do Aluno", NULL=False, default=timezone.now(),)
+    dtfinal = models.DateField(help_text="Informe a Data Final do Aluno", NULL=True, blank=True)
     nome = models.CharField(max_length=70, help_text="Informe o Nome do Aluno")
 
     def __str__(self):
